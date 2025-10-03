fff178f1bef26c5648dafa360003e0f4ee753af7
Hibhai Lofat <46946907-guru5ghantal@users.noreply.replit.com>
2025-10-03 13:49:44 +0000

    V

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: PORT,
-    allowedHosts: ["all", "localhost", "152638dc-b8ae-436b-948a-2ccb92e5cfd0-00-1gs50vzghontc.sisko.replit.dev"]
+    allowedHosts: ["all", "3281f93f-953b-4f8d-9891-f0872c1c0f59-00-1c9jj1lkdqiqj.pike.replit.dev"]
   },
 });
